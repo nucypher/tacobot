ab65a5abe2c67dc54e551d6c335a65b6e8113734
derekpierre <derek.pierre@gmail.com>
2023-11-20 11:14:07 -0500

    Update use of TACo domains.

--- agents.py
+++ agents.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 import aiohttp
 from nucypher.blockchain.eth.agents import ContractAgency, CoordinatorAgent
-from nucypher.blockchain.eth.domains import TACoDomain
+from nucypher.blockchain.eth import domains
 from nucypher.blockchain.eth.registry import ContractRegistry
 
 from constants import BASE_URL
@@ -10,10 +10,10 @@ from constants import BASE_URL
 __AGENTS = defaultdict(defaultdict)
 
 _TRACK = {
-    TACoDomain.LYNX: (
+    domains.LYNX: (
         CoordinatorAgent,
     ),
-    TACoDomain.TAPIR: (
+    domains.TAPIR: (
         CoordinatorAgent,
     )
 }
@@ -21,8 +21,8 @@ _TRACK = {
 
 def get_agent(contract_name: str, domain_name: str):
     """Get the agent by domain name."""
-    domain_info = TACoDomain.get_domain_info(domain_name)
-    return __AGENTS[domain_info][contract_name]
+    domain = domains.get_domain(domain_name)
+    return __AGENTS[domain][contract_name]
 
 
 async def fetch_registry(domain):
@@ -35,15 +35,15 @@ async def fetch_registry(domain):
 
 def cache_agents(endpoint: str):
     """Cache agents."""
-    registries = {domain: ContractRegistry.from_latest_publication(domain=domain.name) for domain in _TRACK}
+    registries = {domain: ContractRegistry.from_latest_publication(domain=domain) for domain in _TRACK}
 
-    for domain_info, registry in registries.items():
-        for domain_name, agent_classes in _TRACK.items():
+    for domain, registry in registries.items():
+        for _, agent_classes in _TRACK.items():
             for agent_class in agent_classes:
                 _agent = ContractAgency.get_agent(
                     agent_class=agent_class,
                     registry=registry,
                     blockchain_endpoint=endpoint,
                 )
-                __AGENTS[domain_info][_agent.contract_name.lower()] = _agent
+                __AGENTS[domain][_agent.contract_name.lower()] = _agent
     return __AGENTS
